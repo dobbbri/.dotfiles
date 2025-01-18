69fb6f38f5cb661aac8c4d39d5dfdfa7a21a6c48
Sergio Dobri <sergiodobri@gmail.com>
2025-01-17 22:55:19 -0300

    fix

--- scripts/.config/scripts/set_4d_lightdm.sh
+++ scripts/.config/scripts/set_4d_lightdm.sh
@@ -5,8 +5,8 @@ if [[ ! -d /$HOME/.local/share/dwm ]]; then
   mkdir /usr/share/xsessions/
 fi
 
-sudo mv /usr/share/xsessions/dwm.desktop /usr/share/xsessions/dwm.desktop.bkp
-sudo cp $HOME/.dotfiles/scripts/.config/scripts/config/dwm.desktop /usr/share/xsessions/dwm.desktop
+# sudo mv /usr/share/xsessions/dwm.desktop /usr/share/xsessions/dwm.desktop.bkp
+# sudo cp $HOME/.dotfiles/scripts/.config/scripts/config/dwm.desktop /usr/share/xsessions/dwm.desktop
 sudo mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.bkp
 sudo cp $HOME/.dotfiles/scripts/.config/scripts/config/lightdm.conf /etc/lightdm/lightdm.conf
 sudo mv /etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf.bkp
