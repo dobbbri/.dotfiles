425516109afa54df2307cb69291fcdebd3500337
Sergio Dobri <sergiodobri@gmail.com>
2025-02-09 16:56:45 -0300

    add script to install lazygit

--- scripts/.config/scripts/set_11_deb_download.sh
+++ scripts/.config/scripts/set_11_deb_download.sh
@@ -19,6 +19,12 @@ echo "microsoft edge -----------------------------------------------------------
 wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_132.0.2957.115-1_amd64.deb
 sudo dpkg -i microsoft-edge-stable_132.0.2957.115-1_amd64.deb
 
+echo "lazygit -----------------------------------------------------------"
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+chmod +x lazygit
+sudo mv lazygit /usr/local/bin/
 
 echo "bitwarden -----------------------------------------------------------"
 xdg-open https://bitwarden.com/download/
