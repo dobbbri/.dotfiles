f28d828c4c199914594d9791ffba0c7da7d93b17
Sergio Dobri <sergiodobri@gmail.com>
2025-04-01 11:20:18 -0300

    update...

--- tinyvim/.config/nvim/lua/plugins/configs/blink.lua
+++ tinyvim/.config/nvim/lua/plugins/configs/blink.lua
@@ -9,7 +9,7 @@ return {
     keyword = { range = "full" },
     accept = { auto_brackets = { enabled = false } },
     list = { selection = { preselect = false, auto_insert = true } },
-    menu = { auto_show = true },
+    menu = { auto_show = true, border = "none" },
     documentation = { auto_show = true, auto_show_delay_ms = 500 },
     ghost_text = { enabled = true },
   },
--- tinyvim/.config/nvim/lua/plugins/configs/mini.lua
+++ tinyvim/.config/nvim/lua/plugins/configs/mini.lua
@@ -17,17 +17,7 @@ require("mini.files").setup({
   mappings = { go_in_plus = "<CR>", go_out_plus = "<left>" },
 })
 
-require("mini.indentscope").setup({
-  symbol = "┊",
-  options = { try_as_border = true },
-})
-
-require("mini.move").setup({
-  mappings = { left = "H", right = "L", down = "J", up = "K" },
-})
-
-require("mini.pairs").setup({
-  modes = { insert = true, command = true, terminal = true },
-})
-
+require("mini.move").setup({ mappings = { left = "H", right = "L", down = "J", up = "K" }})
+require("mini.pairs").setup({ modes = { insert = true, command = true, terminal = true }})
+require("mini.indentscope").setup({symbol = "┊"})
 require("mini.icons").setup()
