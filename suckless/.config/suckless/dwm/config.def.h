db6762a0e397da6e9eff1c52b6bd113e2580062b
Sergio Dobri <sergiodobri@gmail.com>
2025-07-03 14:45:47 -0300

    update...

--- suckless/.config/suckless/dwm/config.def.h
+++ suckless/.config/suckless/dwm/config.def.h
@@ -35,14 +35,27 @@ static const char *const autostart[] = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",             NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",          NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "GitHub Desktop",   NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "mpv",  				    NULL,       NULL,       0,       		  1,           -1 },
+	{ "qimgv",    			  NULL,       NULL,       0,       		  1,           -1 },
+	{ "Transmission-gtk", NULL,       NULL,       0,       		  1,           -1 },
+	{ "Lxappearance",   	NULL,       NULL,       0,       		  1,           -1 },
+	{ "Pavucontrol",  		NULL,       NULL,       0,       		  1,           -1 },
+	{ "PCManFM", 			    NULL,       NULL,       0,       		  0,           -1 },
+  { "st",               NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -101,8 +114,8 @@ static const Key keys[] = {
 
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_k,      movestack,      {.i = -1 } },
 
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
