35925565f4be05a4df8840ba015e7b8fe3611de9
Sergio Dobri <sergiodobri@gmail.com>
2025-07-27 17:38:11 -0300

    update...

--- scripts/.config/scripts/set_7_dev_sites.sh
+++ scripts/.config/scripts/set_7_dev_sites.sh
@@ -17,7 +17,10 @@ git clone git@github.com:dobbbri/terapeuta_luciana_2025.git
 git clone git@github.com:elisapetcare/epc_in_dev.git
 git clone git@github.com:dobbbri/pousada_do_tucunare_2020.git
 git clone git@github.com:dobbbri/elisa_pet_care_v2.git
+git clone git@github.com:dobbbri/epc_in_dev_v2_audios_colors.git
+
 git clone git@github.com:WhitePaper233/yukina.git
 git clone git@github.com:mearashadowfax/ScrewFast.git
+git clone git@github.com:area44/astro-audionaut.git
 
 cd /home/sdobri
