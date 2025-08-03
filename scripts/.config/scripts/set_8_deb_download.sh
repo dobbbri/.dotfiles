0f853f37e18ff4fd81fae144b5a4973cbd0669b1
Sergio Dobri <sergiodobri@gmail.com>
2025-08-03 18:52:13 -0300

    update...

--- scripts/.config/scripts/set_8_deb_download.sh
+++ scripts/.config/scripts/set_8_deb_download.sh
@@ -5,12 +5,12 @@
 # https://github.com/shiftkey/desktop/release
 
 cd $HOME/Downloads/
-sudo apt install fonts-liberation -yy
 
-echo "librewolf browser --------------------------------------------------------"
-sudo apt update && sudo apt install extrepo -y
-sudo extrepo enable librewolf
-sudo apt update && sudo apt install librewolf -y
+# echo "librewolf browser --------------------------------------------------------"
+# sudo apt install fonts-liberation -yy
+# sudo apt update && sudo apt install extrepo -y
+# sudo extrepo enable librewolf
+# sudo apt update && sudo apt install librewolf -y
 
 # echo "min browser --------------------------------------------------------------"
 # wget https://github.com/minbrowser/min/releases/download/v1.33.1/min-1.33.1-amd64.deb
@@ -19,6 +19,10 @@ sudo apt update && sudo apt install librewolf -y
 # echo "gitHub desktop -----------------------------------------------------------"
 # wget https://github.com/shiftkey/desktop/releases/download/release-3.4.8-linux1/GitHubDesktop-linux-amd64-3.4.8-linux1.deb
 # sudo dpkg -i GitHubDesktop-linux-amd64-3.4.8-linux1.deb
+
+echo "fastfetch -------------------------------------------------------------------"
+wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.49.0/fastfetch-linux-amd64.deb
+sudo dpkg -i fastfetch-linux-amd64.deb
 
 echo "microsoft edge -----------------------------------------------------------"
 wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_132.0.2957.115-1_amd64.deb
