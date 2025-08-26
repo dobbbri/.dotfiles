10ad9fad3d720195089d52cac5ff36d502a02435
Sergio Dobri <sergiodobri@gmail.com>
2025-08-26 17:20:24 -0300

    update...

--- newvim/.config/nvim/lsp/lua_ls.lua
+++ newvim/.config/nvim/lsp/lua_ls.lua
@@ -12,22 +12,22 @@ return {
     '.git',
   },
   settings = {
-		Lua = {
-			runtime = {
-				version = 'LuaJIT',
-			},
-			diagnostics = {
-				globals = {
-					'vim',
-					'require'
-				},
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
