706987f3f1adc20e4979bedf5666011ae5610d2d
Sergio Dobri <sergiodobri@gmail.com>
2025-08-29 12:33:15 -0300

    replace mini.files to oil.nvim

--- newvim/.config/nvim/plugin/mini.lua
+++ newvim/.config/nvim/plugin/mini.lua
@@ -1,18 +1,5 @@
 vim.pack.add({ "https://github.com/echasnovski/mini.nvim" })
 
-local ignore_files = {
-  ".DS_Store",
-  ".git",
-  ".astro",
-  ".vscode",
-  ".gitignore",
-  -- "dist",
-  "lazy-lock.json",
-  "package-lock.json",
-  "node_modules",
-  "pnpm-lock.yaml",
-}
-
 local win_config = function()
   local height = math.floor(0.618 * vim.o.lines)
   local width = math.floor(0.618 * vim.o.columns)
@@ -29,18 +16,12 @@ require("mini.diff").setup({
   config = { view = { style = vim.go.number and 'number' } }
 })
 
-require("mini.files").setup({
-  content = { filter = function(entry) return not vim.tbl_contains(ignore_files, entry.name) end },
-  mappings = { close = "q", go_in_plus = "<CR>", go_out_plus = "<left>" },
-})
-vim.keymap.set("n", "-", function() require("mini.files").open() end, { desc = "Show File Manager" })
-vim.keymap.set("n", "<leader>e", function() require("mini.files").open() end, { desc = "Show File Manager" })
-
 require("mini.move").setup({
   mappings = { left = "H", right = "L", down = "J", up = "K" }
 })
 
 require("mini.pairs").setup()
+require("mini.icons").setup()
 
 require("mini.pick").setup({
   window = { config = win_config() }
@@ -49,3 +30,25 @@ vim.keymap.set("n", "<leader>f", "<cmd>Pick files<CR>", { desc = "List files" })
 vim.keymap.set("n", "<leader>m", "<cmd>Pick resume<CR>", { desc = "List Pick resume" })
 vim.keymap.set("n", "<leader>w", "<cmd>Pick grep_live<CR>", { desc = "Search by word" })
 vim.keymap.set("n", "<leader><space>", "<cmd>Pick buffers<CR>", { desc = "List opened buffers" })
+
+
+-- local ignore_files = {
+--   ".DS_Store",
+--   ".git",
+--   ".astro",
+--   ".vscode",
+--   ".gitignore",
+--   -- "dist",
+--   "lazy-lock.json",
+--   "package-lock.json",
+--   "node_modules",
+--   "pnpm-lock.yaml",
+-- }
+
+-- require("mini.files").setup({
+--   content = { filter = function(entry) return not vim.tbl_contains(ignore_files, entry.name) end },
+--   mappings = { close = "q", go_in_plus = "<CR>", go_out_plus = "<left>" },
+-- })
+-- vim.keymap.set("n", "-", function() require("mini.files").open() end, { desc = "Show File Manager" })
+-- vim.keymap.set("n", "<leader>e", function() require("mini.files").open() end, { desc = "Show File Manager" })
+
---
+++ newvim/.config/nvim/plugin/oil.lua
@@ -0,0 +1,42 @@
+vim.pack.add({
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/benomahony/oil-git.nvim" }
+})
+
+local ignore_files = {
+  ".DS_Store",
+  ".git",
+  ".astro",
+  ".vscode",
+  ".gitignore",
+  -- "dist",
+  "lazy-lock.json",
+  "package-lock.json",
+  "node_modules",
+  "pnpm-lock.yaml",
+}
+
+require("oil").setup {
+  columns = { "icon" },
+  keymaps = {
+    ["<C-h>"] = false,
+    ["<C-l>"] = false,
+    ["<C-k>"] = false,
+    ["<C-j>"] = false,
+    ["<M-h>"] = "actions.select_split",
+    ["q"] = { "actions.close", mode = "n" },
+  },
+  float = {
+    max_width = 0.4,
+    max_height = 0,
+    border = "single"
+  },
+  view_options = {
+    show_hidden = true,
+    is_always_hidden = function(name, _)
+      return vim.tbl_contains(ignore_files, name)
+    end,
+  },
+}
+
+vim.keymap.set("n", "-", require("oil").toggle_float, { desc = "Show File Manager" })
