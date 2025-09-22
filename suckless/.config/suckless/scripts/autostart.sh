744e9ee170a3664bbaca12ed09c9e2ea585a50a4
Sergio Dobri <sergiodobri@gmail.com>
2025-09-22 17:12:59 -0300

    update...

--- suckless/.config/suckless/scripts/autostart.sh
+++ suckless/.config/suckless/scripts/autostart.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 slstatus &
-feh --bg-scale ~/.dotfiles/pictures/fabrizio-conti-c3wsMnxQZDw-unsplash.jpg &
+
+hsetroot -cover ~/.dotfiles/pictures/fabrizio-conti-c3wsMnxQZDw-unsplash.jpg &
+# feh --bg-scale ~/.dotfiles/pictures/fabrizio-conti-c3wsMnxQZDw-unsplash.jpg &
 
 # syncthing serve --no-browser --no-restart --logflags=0 &
 
--- suckless/.config/suckless/slstatus/config.h
+++ suckless/.config/suckless/slstatus/config.h
@@ -72,12 +72,12 @@ const size_t notifiable_levels_count = sizeof(notifiable_levels) / sizeof(notifi
  */
 static const struct arg args[] = {
 	/* function format                                argument */
-  { wifi_essid,     "  ^c#aa00ff^󰖩 ^c#cccccc^%s",   "wlp2s0b1" },
+  { battery_state,  "  ^c#6CF982^%s",               "BAT0"  },
+  { battery_perc,   "^c#6CF982^ ^c#cccccc^%s%%",   "BAT0"  },
   { ram_perc,       "  ^c#4fc3f7^ ^c#cccccc^%s%%", NULL    },
-	{ battery_state,  "  ^c#6CF982^%s",               "BAT0"  },
-	{ battery_perc,   "^c#6CF982^ ^c#cccccc^%s%%",   "BAT0"  },
-	{ datetime,       "  ^c#ffc107^󰥔 ^c#cccccc^%s ",  "%H:%M" },
+  { wifi_essid,     "  ^c#aa00ff^󰖩 ^c#cccccc^%s",   "wlp2s0b1" },
+	{ datetime,       "  ^c#FF5555^󰥔 ^c#cccccc^%s ",  "%H:%M" },
 	{ battery_notify, "",                             "BAT0"  }, 
   // { wifi_perc,      "^c#aa00ff^ 󰖩 ^c#cccccc^%s%% ", "wlp2s0b1" },
-  // { datetime,       "^c#FF5555^ 󰸗 ^c#cccccc^%s",    "%d/%m" },
+  // { datetime,       "^c#ffc107^ 󰸗 ^c#cccccc^%s",    "%d/%m" },
 };
