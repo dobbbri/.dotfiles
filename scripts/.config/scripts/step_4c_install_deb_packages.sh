531f8850e873e4a0242c27b9006378c529ca4b91
Sergio Dobri <sergiodobri@gmail.com>
2025-11-03 06:21:23 -0300

    update...

--- scripts/.config/scripts/step_4c_install_deb_packages.sh
+++ scripts/.config/scripts/step_4c_install_deb_packages.sh
@@ -50,8 +50,7 @@ sudo mv lazygit /usr/local/bin/
 # wget https://github.com/minbrowser/min/releases/download/v1.33.1/min-1.33.1-amd64.deb
 # sudo dpkg -i min-1.33.1-amd64.deb
 
-# echo "gitHub desktop -----------------------------------------------------------"
-# wget -qO - https://mirror.mwt.me/shiftkey-desktop/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/mwt-desktop.gpg > /dev/null
-# sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/mwt-desktop.gpg] https://mirror.mwt.me/shiftkey-desktop/deb/ any main" > /etc/apt/sources.list.d/mwt-desktop.list'
-#
-# sudo apt update && sudo apt install github-desktop
+echo "gitHub desktop -----------------------------------------------------------"
+wget -qO- "https://mirror.mwt.me/shiftkey-desktop/install.sh" | sudo -s
+
+sudo apt update && sudo apt install github-desktop
