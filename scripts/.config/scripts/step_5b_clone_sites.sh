fe62ef36d8181fa3555ea4246acbd6f2843df4c1
Sergio Dobri <sergiodobri@gmail.com>
2025-11-05 11:40:53 -0300

    update...

--- scripts/.config/scripts/step_5b_clone_sites.sh
+++ scripts/.config/scripts/step_5b_clone_sites.sh
@@ -14,7 +14,9 @@ git clone git@github.com:dobbbri/videira-blog-2025.git
 git clone git@github.com:dobbbri/espaco_videira_2025.git
 git clone git@github.com:dobbbri/terapeuta_luciana_2025.git
 
+git clone git@github.com:dobbbri/pousada_do_tucunare_2026.git
 git clone git@github.com:dobbbri/pousada_do_tucunare_2020.git
+git clone git@github.com:dobbbri/pousada_do_tucunare.git
 
 git clone git@github.com:elisapetcare/epc_in_dev.git
 git clone git@github.com:dobbbri/elisa_pet_care_v2.git
--- scripts/.config/scripts/step_5c_clone_dev.sh
+++ scripts/.config/scripts/step_5c_clone_dev.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
-# git clone git@github.com:dobbbri/dotfiles.git
-# git clone git@github.com:elisapetcare/epc_in_dev.git
-# git clone git@github.com:dobbbri/elisa_pet_care_v2.git
-# git clone git@github.com:dobbbri/Projects.git
-
 echo "Add Sites Folder -----------------------------------------------------------"
-mkdir -p /home/sdobri/Sites
+mkdir -p /home/sdobri/Dev/
 
-cd /home/sdobri/Sites
+cd /home/sdobri/Dev/
 
-git clone git@github.com:dobbbri/blog.git
-git clone git@github.com:dobbbri/videira-blog-2025.git
-git clone git@github.com:dobbbri/espaco_videira_2025.git
-git clone git@github.com:dobbbri/terapeuta_luciana_2025.git
-
-git clone git@github.com:dobbbri/pousada_do_tucunare_2020.git
-
-git clone git@github.com:elisapetcare/epc_in_dev.git
-git clone git@github.com:dobbbri/elisa_pet_care_v2.git
-git clone git@github.com:dobbbri/epc_in_dev_v2_audios_colors.git
-git clone git@github.com:elisapetcare/elisa_pet_care_lp_2025.git
-
-git clone git@github.com:WhitePaper233/yukina.git
 git clone git@github.com:mearashadowfax/ScrewFast.git
 git clone git@github.com:area44/astro-audionaut.git
 
-git clone https://github.com/dobbbri/astro-blog-feather-template
+git clone git@github.com:dobbbri/astro-paper-blog-my.git
+git clone git@github.com:dobbbri/astro-blog-feather-template
+git clone git@github.com:dobbbri/astro-blog-blogsmith-pro-template.git 
+git clone git@github.com:dobbbri/astro-blog-fuwari.git
+git clone git@github.com:dobbbri/astro-blog-yukina-template.git
+
+git clone git@github.com:dobbbri/tina_cms_blog.git
+git clone git@github.com:dobbbri/pagescms_astro.git
+git clone git@github.com:dobbbri/decapcms_astro.git
 
 cd /home/sdobri
