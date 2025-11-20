78de2c834c9787efe27457bf6f6ea5fec2d77ce0
Sergio Dobri <sergiodobri@gmail.com>
2025-11-20 01:41:56 -0300

    update...

--- suckless/.config/suckless/dwm/config.h
+++ suckless/.config/suckless/dwm/config.h
@@ -13,7 +13,7 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const char *fonts[]          = { "UbuntuSansMono Nerd Font:size=12:antialias=true", "monospace:size=12:antialias=true" };
+static const char *fonts[]          = { "UbuntuSansMono Nerd Font:style=medium:size=12:antialias=true", "monospace:size=12:antialias=true" };
 static const char dmenufont[]       = "UbuntuSansMono Nerd Font:size=12";
 
 static const char col_gray1[]       = "#000000";
--- suckless/.config/suckless/scripts/autostart.sh
+++ suckless/.config/suckless/scripts/autostart.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Set keyboard repeat rate
+xset r rate 250 60
+
+# Fix cursor
+xsetroot -cursor_name left_ptr
+
+# Set root window background color
+# xsetroot -solid black
+
 slstatus &
 
 hsetroot -cover ~/.dotfiles/pictures/plasma1366x768.png &
@@ -7,11 +16,9 @@ hsetroot -cover ~/.dotfiles/pictures/plasma1366x768.png &
 # /usr/bin/syncthing serve --no-browser --no-restart --logflags=0 &
 
 # polkit
-lxpolkit &
+# lxpolkit &
 
 dunst -config ~/.config/suckless/dunst/dunstrc &
 
 picom --config ~/.config/suckless/picom/picom.conf &
 
-# Fix cursor
-xsetroot -cursor_name left_ptr
