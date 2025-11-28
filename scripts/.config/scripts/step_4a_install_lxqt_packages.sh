b2aea548e3067d22411999dfa2bc35d4953464d3
Sergio Dobri <sergiodobri@gmail.com>
2025-11-28 14:11:49 -0300

    update...

--- scripts/.config/scripts/step_4a_install_lxqt_packages.sh
+++ scripts/.config/scripts/step_4a_install_lxqt_packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt install -y lxqt-core xorg-dev xbindkeys xinput hsetroot brightnessctl \
-  xdotool unzip libnotify-dev curl unzip ssh-askpass wget stow papirus-icon-theme \
-  xdg-user-dirs-gtk build-essential cmake meson ninja-build wget \
+sudo apt install -y  lxqt-core xorg xinit xorg-dev xbindkeys xinput hsetroot brightnessctl \
+  xdotool unzip curl unzip ssh-askpass wget stow papirus-icon-theme \
+  build-essential cmake meson ninja-build wget \
   pkg-config || echo "Warning: Package installation failed."
 
 echo "Package installation completed."
