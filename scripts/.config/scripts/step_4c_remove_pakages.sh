78fe8f67420169414d8d5b2ecb4bd7076c7fbec6
Sergio Dobri <sergiodobri@gmail.com>
2025-11-28 10:24:37 -0300

    update...

--- scripts/.config/scripts/step_4c_remove_pakages.sh
+++ scripts/.config/scripts/step_4c_remove_pakages.sh
@@ -3,7 +3,7 @@
 echo "-Enable/Fix tap to click"
 sudo apt remove --yes xserver-xorg-input-synaptics
 sudo apt install --yes xserver-xorg-input-libinput
-sudo cp /home/sdobri/.dotfiles/scripts/.config/scripts/config/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
+sudo cp ~/.dotfiles/scripts/.config/scripts/config/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
 
 echo "remove modemmanager"
 sudo apt autoremove modemmanager -y
@@ -16,3 +16,10 @@ sudo apt autoremove blueman bluez-utils bluez bluetooth -y
 
 echo "Remove xdg-desktop-portal-gtk"
 sudo apt autoremove xdg-desktop-portal-gtk -y
+
+echo "Remove pulseaudio"
+systemctl --user stop pulseaudio.service pulseaudio.socket
+systemctl --user disable pulseaudio.service pulseaudio.socket
+systemctl --user mask pulseaudio.service
+sudo apt autoremove pulseaudio pulseaudio-utils gstreamer1.0-pulseaudio libpulse0 libpulse-mainloop-glib0 libpulsedsp -y
+rm -rf ~/.config/pulse ~/.pulse ~/.pulse-cookie
