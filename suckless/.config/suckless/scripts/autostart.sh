476a35b36fe5152ab3b44d4bcceb3a40b1e3e90a
Sergio Dobri <sergiodobri@gmail.com>
2025-12-12 23:41:01 -0300

    update...

--- suckless/.config/suckless/scripts/autostart.sh
+++ suckless/.config/suckless/scripts/autostart.sh
@@ -1,25 +1,16 @@
 #!/bin/sh
 
-# Set keyboard repeat rate
 xset r rate 250 60
 
-# Fix cursor
 xsetroot -cursor_name left_ptr
-
-# Set root window background color
-# xsetroot -solid black
 
 slstatus &
 
 hsetroot -cover ~/.dotfiles/pictures/1-etheral.jpg &
 
-# /usr/bin/syncthing serve --no-browser --no-restart --logflags=0 &
-
-# xfce4-power-manager &
-# xfce4-session &
-# lxpolkit &
-
 dunst -config ~/.config/suckless/dunst/dunstrc &
 
 picom --config ~/.config/suckless/picom/picom.conf &
+
+# /usr/bin/syncthing serve --no-browser --no-restart --logflags=0 &
 
