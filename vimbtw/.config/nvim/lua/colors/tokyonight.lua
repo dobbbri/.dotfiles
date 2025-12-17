12bf1ffc4e545c3a41f334d8fc80562a4d6f1681
Sergio Dobri <sergiodobri@gmail.com>
2025-12-17 03:07:28 -0300

    update...

--- vimbtw/.config/nvim/lua/colors/tokyonight.lua
+++ vimbtw/.config/nvim/lua/colors/tokyonight.lua
@@ -1,7 +1,7 @@
 vim.pack.add({ "https://github.com/folke/tokyonight.nvim" }, { confirm = false })
 
 require("tokyonight").setup({
-  transparent = false,
+  transparent = true,
 })
 
 vim.cmd.colorscheme("tokyonight-night")
